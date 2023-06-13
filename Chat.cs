3ba7ae463cf7b082797a3e172a031dfa7aee34e5
Roman Kornilov <kornilovroman123@gmail.com>
2023-06-13 20:47:21 +0500

    Add basic methods for working with ChatGPT API

---
+++ Chat.cs
@@ -0,0 +1,36 @@
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
+
+namespace chatgpt_bot;
+
+public class Chat
+{
+    private OpenAIAPI _api;
+    private Conversation _chat;
+    private int _msgCount;
+    
+    public Chat(OpenAIAPI api)
+    {
+        _api = api;
+        CreateConversation();
+    }
+
+    public void CreateConversation()
+    {
+        _chat = _api.Chat.CreateConversation();
+        _msgCount = 0;
+    }
+
+    public async IAsyncEnumerable<string> SendMessage(string userInput)
+    {
+        if (_msgCount >= 50)
+            CreateConversation();
+        
+        _chat.AppendUserInput(userInput);
+        _msgCount += 1;
+        await foreach (var res in _chat.StreamResponseEnumerableFromChatbotAsync())
+        {
+            yield return res;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using OpenAI_API;
+namespace chatgpt_bot
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            OpenAIAPI api = new OpenAIAPI(APIAuthentication.LoadFromPath());
+
+            Chat cht = new Chat(api);
+            await foreach (var res in cht.SendMessage("Hello! My name is Heisenberg."))
+            {
+                Console.Write(res);
+            }
+            Console.WriteLine();
+            await foreach (var res in cht.SendMessage("SAY MY NAME."))
+            {
+                Console.Write(res);
+            }
+        }
+    }
+}
