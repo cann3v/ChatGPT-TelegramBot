693e73402173fde4a5b4115e789b1880a454df6b
Roman Kornilov <kornilovroman123@gmail.com>
2023-08-06 15:18:55 +0500

    Delete unused parts of code

--- Chat.cs
+++ Chat.cs
@@ -9,7 +9,6 @@ public class Chat
 {
     private OpenAIAPI _api;
     private Conversation _chat;
-    private int _msgCount;
     private static readonly ILog Log = LogManager.GetLogger(typeof(Chat));
     
     public Chat(OpenAIAPI api)
@@ -21,16 +20,11 @@ public class Chat
     public void CreateConversation()
     {
         _chat = _api.Chat.CreateConversation();
-        _msgCount = 0;
     }
 
     public async IAsyncEnumerable<string> SendMessage(string userInput)
     {
-        if (_msgCount >= 50)
-            CreateConversation();
-        
         _chat.AppendUserInput(userInput);
-        _msgCount += 1;
         await foreach (var res in _chat.StreamResponseEnumerableFromChatbotAsync())
         {
             yield return res;
